0e023848990927b0a8f5576ac8cc19460d46cc14
Tom Deseyn <tom.deseyn@gmail.com>
2018-07-09 11:03:25 +0200

    Add fail console project

---
+++ console-2.1-fail/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_2._1_fail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new Exception("Exception thrown from console project");
+        }
+    }
+}
